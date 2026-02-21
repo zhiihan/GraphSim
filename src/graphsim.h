ea6382c03eb9f16b30870d7409b3630f66fad7a0
Zhihua Han <zhihuahan72@gmail.com>
2026-02-21 13:28:19 +0100

    chore: formatting

--- src/graphsim.h
+++ src/graphsim.h
@@ -10,7 +10,7 @@ graphsim version 0.10, dated 2005-Jan-27
 
 See the article
 
-  S. Anders, H. J. Briegel: 
+  S. Anders, H. J. Briegel:
   Fast simulation of Stabilizer Circuits using a Graph States Formalism
   quant-ph/0504117
 
@@ -29,7 +29,7 @@ for a description of this software.
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this file; see the file COPYING.  If not, browse to 
+// along with this file; see the file COPYING.  If not, browse to
 // http://www.fsf.org/licenses/gpl.html
 
 */
@@ -53,7 +53,7 @@ file and the namespace identifier.
 #ifndef GRAPHSIM_H
 #define GRAPHSIM_H
 
-//The following directives are for SWIG, see http://www.swig.org
+// The following directives are for SWIG, see http://www.swig.org
 #ifdef SWIG
 %module (docstring="Graph State Stabilizer Simulator -- S. Anders") graphsim
 %{
@@ -71,16 +71,16 @@ file and the namespace identifier.
 %include "stabilizer.h"
 #endif // SWIG
 
-#include <iostream>
-#include <cstdlib>
-#include <vector>
 #include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
 #include "loccliff.h"
 #include "stabilizer.h"
 
-#include <cstring>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -89,75 +89,75 @@ auch an index, the type VertexIndex (which is just unsigned long) is always
 used */
 typedef unsigned long VertexIndex;
 
-/*! A GraphRegister object maintains a list of its vertices (qubits), each 
+/*! A GraphRegister object maintains a list of its vertices (qubits), each
 described by an object of the class QubitVertex described here.*/
 struct QubitVertex {
-   /*!byprod is the vertex operator (VOp) associated with the qubit (the name 
-   stems from the term 'byproduct operator' used for the similar concept in 
-   the one-way quantum computer.*/
-   LocCliffOp byprod;
-   /*! neigbors is the adjacency list for this vertex */
-   unordered_set<VertexIndex> neighbors;
-   /*! Upon construction, a qubit vertex is initialised with the Hadamard
-   operation as VOp, and with wmpty neighbor list. This makes it represent
-   a |0>. */
-   QubitVertex (void)
-     : byprod (lco_H) {};
+  /*!byprod is the vertex operator (VOp) associated with the qubit (the name
+  stems from the term 'byproduct operator' used for the similar concept in
+  the one-way quantum computer.*/
+  LocCliffOp byprod;
+  /*! neigbors is the adjacency list for this vertex */
+  unordered_set<VertexIndex> neighbors;
+  /*! Upon construction, a qubit vertex is initialised with the Hadamard
+  operation as VOp, and with wmpty neighbor list. This makes it represent
+  a |0>. */
+  QubitVertex(void) : byprod(lco_H){};
 };
-   
+
 #ifndef SWIG
 /*! This structure is only for internal use for the cphase functions. */
 struct ConnectionInfo {
-   bool wasEdge;
-   bool non1;
-   bool non2;
+  bool wasEdge;
+  bool non1;
+  bool non2;
 };
 #endif
 
 //! A quantum register.
-/*! GraphRegister is the central class of graphsim. It represents a register of qubits
-that can be entangled with each other. It offers functions to initialize the register,
-let gates operate on the qubits, do measurements and print out the state. */
+/*! GraphRegister is the central class of graphsim. It represents a register of
+qubits that can be entangled with each other. It offers functions to initialize
+the register, let gates operate on the qubits, do measurements and print out the
+state. */
 class GraphRegister {
-  public:
-   /*! This vector stores all the qubits, represented as QubitVertex objects. The index
-   of the vector is usually taken as of type VertexIndex. */
-   vector<QubitVertex> vertices;
-   GraphRegister (VertexIndex numQubits, int randomize = -1);
-   GraphRegister (GraphRegister& gr);
-   ~GraphRegister () {};
-   void local_op (VertexIndex v, LocCliffOp o);
-   void hadamard (VertexIndex v);
-   void phaserot (VertexIndex v);
-   void bitflip (VertexIndex v);
-   void phaseflip (VertexIndex v);
-   void cphase (VertexIndex v1, VertexIndex v2);      
-   void cnot (VertexIndex vc, VertexIndex vt);
-   int measure (VertexIndex v, LocCliffOp basis = lco_Z, 
-      bool* determined = NULL, int force = -1);
-   Stabilizer & get_full_stabilizer (void) const;
-   void invert_neighborhood (VertexIndex v);
-   void print_adj_list (ostream& os = cout) const;
-   void print_adj_list_line (ostream& os, VertexIndex i) const;
-   void print_stabilizer (ostream& os = cout) const;
-   vector<vector<unsigned long>> adjacency_matrix() const;
-   vector<unordered_set<unsigned long>> adjacency_list() const;
-   vector<string> vop_list() const;
-   vector<string> stabilizer_list() const;
-   private:
-   void add_edge (VertexIndex v1, VertexIndex v2);
-   void del_edge (VertexIndex v1, VertexIndex v2);
-   void toggle_edge (VertexIndex v1, VertexIndex v2);
-   int graph_Z_measure (VertexIndex v, int force = -1);
-   int graph_Y_measure (VertexIndex v, int force = -1);
-   int graph_X_measure (VertexIndex v, bool* determined = NULL, int force = -1);
-   void toggle_edges (const unordered_set<VertexIndex> vs1, 
-      const unordered_set<VertexIndex> vs2);      
-   bool remove_byprod_op (VertexIndex v, VertexIndex use_not);
-   void cphase_with_table (VertexIndex v1, VertexIndex v2);
-   ConnectionInfo getConnectionInfo (VertexIndex v1, VertexIndex v2);  
-};
+public:
+  /*! This vector stores all the qubits, represented as QubitVertex objects. The
+  index of the vector is usually taken as of type VertexIndex. */
+  vector<QubitVertex> vertices;
+  GraphRegister(VertexIndex numQubits, int randomize = -1);
+  GraphRegister(GraphRegister &gr);
+  ~GraphRegister(){};
+  void local_op(VertexIndex v, LocCliffOp o);
+  void hadamard(VertexIndex v);
+  void phaserot(VertexIndex v);
+  void bitflip(VertexIndex v);
+  void phaseflip(VertexIndex v);
+  void cphase(VertexIndex v1, VertexIndex v2);
+  void cnot(VertexIndex vc, VertexIndex vt);
+  int measure(VertexIndex v, LocCliffOp basis = lco_Z, bool *determined = NULL,
+              int force = -1);
+  Stabilizer &get_full_stabilizer(void) const;
+  void invert_neighborhood(VertexIndex v);
+  void print_adj_list(ostream &os = cout) const;
+  void print_adj_list_line(ostream &os, VertexIndex i) const;
+  void print_stabilizer(ostream &os = cout) const;
+  vector<vector<unsigned long>> adjacency_matrix() const;
+  vector<unordered_set<unsigned long>> adjacency_list() const;
+  vector<string> vop_list() const;
+  vector<string> stabilizer_list() const;
 
+private:
+  void add_edge(VertexIndex v1, VertexIndex v2);
+  void del_edge(VertexIndex v1, VertexIndex v2);
+  void toggle_edge(VertexIndex v1, VertexIndex v2);
+  int graph_Z_measure(VertexIndex v, int force = -1);
+  int graph_Y_measure(VertexIndex v, int force = -1);
+  int graph_X_measure(VertexIndex v, bool *determined = NULL, int force = -1);
+  void toggle_edges(const unordered_set<VertexIndex> vs1,
+                    const unordered_set<VertexIndex> vs2);
+  bool remove_byprod_op(VertexIndex v, VertexIndex use_not);
+  void cphase_with_table(VertexIndex v1, VertexIndex v2);
+  ConnectionInfo getConnectionInfo(VertexIndex v1, VertexIndex v2);
+};
 
 #ifndef SWIG
 /*! As we often iterate over sublists of GraphRegister::vertices, this
@@ -172,43 +172,32 @@ typedef vector<QubitVertex>::const_iterator VertexIterConst;
 /*! A constant version of VtxIdxIter */
 typedef unordered_set<VertexIndex>::const_iterator VtxIdxIterConst;
 
-
 /*! Apply the local (i.e. single-qubit) operation o on vertex v. */
-inline void GraphRegister::local_op (VertexIndex v, LocCliffOp o) {
-   vertices[v].byprod = o * vertices[v].byprod;
+inline void GraphRegister::local_op(VertexIndex v, LocCliffOp o) {
+  vertices[v].byprod = o * vertices[v].byprod;
 }
 
 /*! Apply a Hadamard gate on vertex v */
-inline void GraphRegister::hadamard (VertexIndex v) {
-   local_op (v, lco_H);
-} 
+inline void GraphRegister::hadamard(VertexIndex v) { local_op(v, lco_H); }
 
 /*! Apply a phaserot gate on vertex v. Phaserot means the gate
 S = |0><0| + i |1><1|. */
-inline void GraphRegister::phaserot (VertexIndex v) {
-   local_op (v, lco_S);
-} 
+inline void GraphRegister::phaserot(VertexIndex v) { local_op(v, lco_S); }
 
 /*! Apply a bitflip gate (i.e. a Pauli X) on vertex v */
-inline void GraphRegister::bitflip (VertexIndex v) {
-   local_op (v, lco_X);
-} 
+inline void GraphRegister::bitflip(VertexIndex v) { local_op(v, lco_X); }
 
 /*! Apply a phaseflip gate (i.e. a Pauli Z) on vertex v */
-inline void GraphRegister::phaseflip (VertexIndex v) {
-   local_op (v, lco_Z);
-} 
+inline void GraphRegister::phaseflip(VertexIndex v) { local_op(v, lco_Z); }
 
-#endif //SWIG
+#endif // SWIG
 
-//#define DEBUGOUTPUT
+// #define DEBUGOUTPUT
 
 #ifdef DEBUGOUTPUT
-   #define DBGOUT(a) cout << a
+#define DBGOUT(a) cout << a
 #else
-   #define DBGOUT(a) 
+#define DBGOUT(a)
 #endif
 
-
-
-#endif //GRAPHSIM_H
+#endif // GRAPHSIM_H
--- src/loccliff.h
+++ src/loccliff.h
@@ -19,7 +19,7 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this file; see the file COPYING.  If not, browse to 
+// along with this file; see the file COPYING.  If not, browse to
 // http://www.fsf.org/licenses/gpl.html
 
 // ----------
@@ -35,37 +35,36 @@
 
 using namespace std;
 
-
 // ----=== RightPhase ===----
 
 //! A phase, which is 0, 90, 180, or 270 degrees.
 struct RightPhase {
-   
-   /*! to be read modulo 4, i.e. & 0x03
-   The field 'ph' indicated the angle with a value 0, 1, 2, or 3,
-   corresponding to 0, 90, 180, or 270 degrees
-   Do not rely on ph<3, always read it out anded with 0x03. */
-   unsigned short ph;
 
-   RightPhase (void);               //!< initializes as 0 degrees
-   RightPhase (unsigned short ph_); //!< takes 0..3 as argument and writes it to ph
-   
-   string get_name (void) const;    //!< returns " +", " i", " -", or "-i"
+  /*! to be read modulo 4, i.e. & 0x03
+  The field 'ph' indicated the angle with a value 0, 1, 2, or 3,
+  corresponding to 0, 90, 180, or 270 degrees
+  Do not rely on ph<3, always read it out anded with 0x03. */
+  unsigned short ph;
+
+  RightPhase(void); //!< initializes as 0 degrees
+  RightPhase(
+      unsigned short ph_); //!< takes 0..3 as argument and writes it to ph
+
+  string get_name(void) const; //!< returns " +", " i", " -", or "-i"
 };
-
 
 // ----=== RightMatrix ===----
 
 #ifndef SWIG
 struct RightMatrix {
-   bool sqrt2norm;
-   bool ampls[2][2];
-   RightPhase phases[2][2];
-   bool apply_on_state (vector<bool>::reference ampl1, 
-      vector<bool>::reference ampl2, RightPhase& ph1, RightPhase& ph2);
+  bool sqrt2norm;
+  bool ampls[2][2];
+  RightPhase phases[2][2];
+  bool apply_on_state(vector<bool>::reference ampl1,
+                      vector<bool>::reference ampl2, RightPhase &ph1,
+                      RightPhase &ph2);
 };
 #endif
-
 
 // ----=== LocCliffOp ===----
 
@@ -77,57 +76,56 @@ struct LocCliffOp {
   unsigned short op;
 
   //! constructor, takes an integer in 0..23.
-  LocCliffOp (unsigned short int op_);
+  LocCliffOp(unsigned short int op_);
 
   //! constructor, takes a sign symbol in 0..3 (for I, X, Y, Z) and a
   /*! permutation symbol 0..5 (for A, B, ..., F) */
-  LocCliffOp (unsigned short int signsymb, unsigned short int permsymb);
-  
+  LocCliffOp(unsigned short int signsymb, unsigned short int permsymb);
+
   //! returns something like e.g. "YC" for Hadamard=Y^C
-  string get_name (void) const;
-  
+  string get_name(void) const;
+
   //! replaces op by trans * op * trans^dagger and returns a phase,
   /*! either +1 or -1 (as RightPhase(0) or RightPhase(2)) */
-  RightPhase conjugate (const LocCliffOp trans);
-  
-  //! returns the Hermitian adjoint of op
-  LocCliffOp herm_adjoint (void) const;
-  
-  //! returns the phase of the multiplication of op1 * op2
-  static RightPhase mult_phase (LocCliffOp op1, LocCliffOp op2);
-  
-  //! returns True if op is XA or YA.
-  bool isXY (void) const;
-  
-  //! returns True if op is an operator diagonal in the computational basis
-  bool is_diagonal (void) const;
-  
-  RightMatrix get_matrix (void) const;
-};
+  RightPhase conjugate(const LocCliffOp trans);
 
+  //! returns the Hermitian adjoint of op
+  LocCliffOp herm_adjoint(void) const;
+
+  //! returns the phase of the multiplication of op1 * op2
+  static RightPhase mult_phase(LocCliffOp op1, LocCliffOp op2);
+
+  //! returns True if op is XA or YA.
+  bool isXY(void) const;
+
+  //! returns True if op is an operator diagonal in the computational basis
+  bool is_diagonal(void) const;
+
+  RightMatrix get_matrix(void) const;
+};
 
 // ----=== operators ===----
 
-#ifndef SWIG   // I should add names for the operators so that SWIG
-               // can handle them
+#ifndef SWIG // I should add names for the operators so that SWIG
+             // can handle them
 
 //! muliplies two local Clifford operators. Not commutative!
-LocCliffOp operator* (LocCliffOp a, LocCliffOp b);
+LocCliffOp operator*(LocCliffOp a, LocCliffOp b);
 
 //! Check two LC operators for equality
-bool operator== (LocCliffOp a, LocCliffOp b);
+bool operator==(LocCliffOp a, LocCliffOp b);
 //! Check two LC operators for inequality
-bool operator!= (LocCliffOp a, LocCliffOp b);
+bool operator!=(LocCliffOp a, LocCliffOp b);
 
 //! Adds two phases (modulo 4).
-RightPhase operator+ (RightPhase ph1, RightPhase ph2);
+RightPhase operator+(RightPhase ph1, RightPhase ph2);
 
 //! Check two phases for equality
-bool operator== (RightPhase ph1, RightPhase ph2);
+bool operator==(RightPhase ph1, RightPhase ph2);
 //! Check two phases for inequality
-bool operator!= (RightPhase ph1, RightPhase ph2);
+bool operator!=(RightPhase ph1, RightPhase ph2);
 
-#endif //SWIG
+#endif // SWIG
 
 // ---=== constants ===---
 
@@ -135,29 +133,27 @@ bool operator!= (RightPhase ph1, RightPhase ph2);
 const unsigned short num_LocCliffOps = 24;
 
 // special local Clifford operators:
-const LocCliffOp lco_Id   = 0;        //!< Identity
-const LocCliffOp lco_X    = 1;        //!< Pauli X
-const LocCliffOp lco_Y    = 2;        //!< Pauli Y 
-const LocCliffOp lco_Z    = 3;        //!< Pauli Z
-const LocCliffOp lco_H    = 10;       //!< Hadamard
-const LocCliffOp lco_spiZ = 5;        //!< Sqrt (+iZ)
-const LocCliffOp lco_smiZ = 6;        //!< Sqrt (-iZ)
-const LocCliffOp lco_spiY = 11;       //!< Sqrt (+iY)
-const LocCliffOp lco_smiY = 9;        //!< Sqrt (-iY)
-const LocCliffOp lco_spiX = 14;       //!< Sqrt (+iX)
-const LocCliffOp lco_smiX = 15;       //!< Sqrt (-iX)
-const LocCliffOp lco_S    = lco_smiZ; //!< Pi/4 phase rot
-const LocCliffOp lco_Sh   = lco_spiZ; //!< herm. conj. of Pi/4 phase rot
+const LocCliffOp lco_Id = 0;        //!< Identity
+const LocCliffOp lco_X = 1;         //!< Pauli X
+const LocCliffOp lco_Y = 2;         //!< Pauli Y
+const LocCliffOp lco_Z = 3;         //!< Pauli Z
+const LocCliffOp lco_H = 10;        //!< Hadamard
+const LocCliffOp lco_spiZ = 5;      //!< Sqrt (+iZ)
+const LocCliffOp lco_smiZ = 6;      //!< Sqrt (-iZ)
+const LocCliffOp lco_spiY = 11;     //!< Sqrt (+iY)
+const LocCliffOp lco_smiY = 9;      //!< Sqrt (-iY)
+const LocCliffOp lco_spiX = 14;     //!< Sqrt (+iX)
+const LocCliffOp lco_smiX = 15;     //!< Sqrt (-iX)
+const LocCliffOp lco_S = lco_smiZ;  //!< Pi/4 phase rot
+const LocCliffOp lco_Sh = lco_spiZ; //!< herm. conj. of Pi/4 phase rot
 
 // the right angles:
-const RightPhase rp_p1 = 0;         //!< +1
-const RightPhase rp_pI = 1;         //!< +i
-const RightPhase rp_m1 = 2;         //!< -1
-const RightPhase rp_mI = 3;         //!< -i
-
+const RightPhase rp_p1 = 0; //!< +1
+const RightPhase rp_pI = 1; //!< +i
+const RightPhase rp_m1 = 2; //!< -1
+const RightPhase rp_mI = 3; //!< -i
 
 // ---=== internals ===---
-
 
 #ifndef SWIG
 
@@ -165,103 +161,72 @@ const RightPhase rp_mI = 3;         //!< -i
 // (all others functions in loccliff.cpp)
 namespace loccliff_tables {
 
-const unsigned short meas_conj_tbl [3] [num_LocCliffOps] = 
-{{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3}, 
- {2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1}, 
- {3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2}};
+const unsigned short meas_conj_tbl[3][num_LocCliffOps] = {
+    {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3},
+    {2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1},
+    {3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2}};
 
-const unsigned short lco_mult_tbl [num_LocCliffOps] [num_LocCliffOps] = 
+const unsigned short lco_mult_tbl[num_LocCliffOps][num_LocCliffOps] =
 #include "multtbl.tbl"
-;    
+    ;
 
-const short adj_tbl [24] =
-   { 0,  1,  2,  3,
-     4,  6,  5,  7,
-     8, 11, 10,  9,
-    12, 13, 15, 14,
-    20, 22, 23, 21,
-    16, 19, 17, 18};    
-    
- const short phase_tbl[4][4] =
-  {{0, 0, 0, 0},
-   {0, 0, 1, 3},
-   {0, 3, 0, 1},
-   {0, 1, 3, 0}};
+const short adj_tbl[24] = {0,  1,  2,  3,  4,  6,  5,  7,  8,  11, 10, 9,
+                           12, 13, 15, 14, 20, 22, 23, 21, 16, 19, 17, 18};
+
+const short phase_tbl[4][4] = {
+    {0, 0, 0, 0}, {0, 0, 1, 3}, {0, 3, 0, 1}, {0, 1, 3, 0}};
 
 } // end namespace loccliff_tables
 
+inline LocCliffOp::LocCliffOp(unsigned short int op_) { op = op_; }
 
-inline LocCliffOp::LocCliffOp (unsigned short int op_) 
-{
-   op = op_;
+inline LocCliffOp::LocCliffOp(unsigned short int signsymb,
+                              unsigned short int permsymb) {
+  assert(signsymb < 4 && permsymb < 6);
+  op = permsymb * 4 + signsymb;
 }
 
-inline LocCliffOp::LocCliffOp (unsigned short int signsymb, 
-      unsigned short int permsymb) 
-{
-   assert (signsymb < 4 && permsymb < 6);
-   op = permsymb * 4 + signsymb;
-}
- 
-inline LocCliffOp LocCliffOp::herm_adjoint (void) const
-{
-   return loccliff_tables::adj_tbl [op];
+inline LocCliffOp LocCliffOp::herm_adjoint(void) const {
+  return loccliff_tables::adj_tbl[op];
 }
 
-inline RightPhase LocCliffOp::mult_phase (LocCliffOp op1, LocCliffOp op2)
-{
-   assert (op1.op <= lco_Z.op && op2.op <= lco_Z.op);
-   return RightPhase (loccliff_tables::phase_tbl[op1.op][op2.op]);
+inline RightPhase LocCliffOp::mult_phase(LocCliffOp op1, LocCliffOp op2) {
+  assert(op1.op <= lco_Z.op && op2.op <= lco_Z.op);
+  return RightPhase(loccliff_tables::phase_tbl[op1.op][op2.op]);
 }
 
 //!
-inline bool LocCliffOp::isXY (void) const
-{
-   return (op == lco_X.op || op == lco_Y.op);
+inline bool LocCliffOp::isXY(void) const {
+  return (op == lco_X.op || op == lco_Y.op);
 }
 
-inline bool LocCliffOp::is_diagonal (void) const
-{
-   return (op == lco_Id.op   || op == lco_Z.op || 
-           op == lco_smiZ.op || op == lco_spiZ.op);
+inline bool LocCliffOp::is_diagonal(void) const {
+  return (op == lco_Id.op || op == lco_Z.op || op == lco_smiZ.op ||
+          op == lco_spiZ.op);
 }
 
-inline LocCliffOp operator* (LocCliffOp a, LocCliffOp b) 
-{ 
-  return LocCliffOp (loccliff_tables::lco_mult_tbl [a.op] [b.op]);
+inline LocCliffOp operator*(LocCliffOp a, LocCliffOp b) {
+  return LocCliffOp(loccliff_tables::lco_mult_tbl[a.op][b.op]);
 }
 
-inline bool operator== (LocCliffOp a, LocCliffOp b) 
-{
-  return a.op == b.op;
+inline bool operator==(LocCliffOp a, LocCliffOp b) { return a.op == b.op; }
+
+inline bool operator!=(LocCliffOp a, LocCliffOp b) { return a.op != b.op; }
+
+inline RightPhase::RightPhase(void) { ph = 0; }
+
+inline RightPhase::RightPhase(unsigned short ph_) { ph = ph_; }
+
+inline RightPhase operator+(RightPhase ph1, RightPhase ph2) {
+  return RightPhase((ph1.ph + ph2.ph) & 0x03);
 }
 
-inline bool operator!= (LocCliffOp a, LocCliffOp b) 
-{
-  return a.op != b.op;
-}
-
-inline RightPhase::RightPhase (void) {
-  ph = 0;
-}
-
-inline RightPhase::RightPhase (unsigned short ph_) {
-  ph = ph_;
-}
-
-inline RightPhase operator+ (RightPhase ph1, RightPhase ph2)
-{
-   return RightPhase ((ph1.ph + ph2.ph) & 0x03);
-}
-
-inline bool operator== (RightPhase a, RightPhase b) 
-{
+inline bool operator==(RightPhase a, RightPhase b) {
   return ((a.ph ^ b.ph) & 0x03) == 0;
 }
-inline bool operator!= (RightPhase a, RightPhase b) 
-{
+inline bool operator!=(RightPhase a, RightPhase b) {
   return ((a.ph ^ b.ph) & 0x03) != 0;
 }
-#endif //SWIG
+#endif // SWIG
 
-#endif //LOCCLIFF_H
+#endif // LOCCLIFF_H
