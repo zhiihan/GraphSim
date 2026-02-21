fe46925fb950d8883c8a0d4e24857aa2e1c5aef8
Zhihua Han <zhihuahan72@gmail.com>
2026-02-21 16:38:23 +0100

    fix: demo.py

--- demo.py
+++ demo.py
@@ -3,20 +3,20 @@ import graphsim
 # we need a quantum register with 7 qubits:
 gr = graphsim.GraphRegister(5)
 
-gr.hadamard(0)
-gr.hadamard(1)
-gr.hadamard(2)
-gr.hadamard(3)
-gr.cphase(0, 1)
-gr.cphase(0, 2)
-gr.cphase(0, 3)
-gr.cphase(0, 4)
+gr.H(0)
+gr.H(1)
+gr.H(2)
+gr.H(3)
+gr.CZ(0, 1)
+gr.CZ(0, 2)
+gr.CZ(0, 3)
+gr.CZ(0, 4)
 
 print(gr.adjacency_matrix())
 print(gr.adjacency_list())
 print(gr.vop_list())
-print(gr.stabilizer_str())
+print(gr.stabilizer_list())
 
-gr.hadamard(0)
+gr.H(0)
 
 print(gr.measure(0), gr.measure(1), gr.measure(2), gr.measure(3), gr.measure(4))
