8a9d56bca3d286d880075c2f8a50034911593194
Zhi Han <zhihuahan72@gmail.com>
2026-02-27 20:39:47 +0100

    chore: add nbconvert

--- tasks.py
+++ tasks.py
@@ -25,7 +25,11 @@ def buildwheel(c):
 
 @task(clean)
 def docs(c, serve=False):
-    c.run("sphinx-apidoc -o docs/api/ --module-first --no-toc --force src/graphsim")
+    c.run("sphinx-apidoc -o docs/api/ --module-first --force src/")
+
+    # MacOS : compile with
+    # clang++ -O3 -Wall -shared -std=c++14 -fPIC $(python3 -m pybind11 --includes) bindings.cpp graphsim.cpp loccliff.cpp stabilizer.cpp -o graphsim$(python3-config --extension-suffix)
+
     if serve:
         c.run("sphinx-autobuild --open-browser docs/ docs/build/")
     else:
